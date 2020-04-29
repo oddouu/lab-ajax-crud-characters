16fd3e8352199368f37d6f9f505ba662fc552e59
Stefano Santonocito <oddouu@gmail.com>
2020-04-29 20:02:52 +0100

    iteration 1 and 2 done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,89 @@ class APIHandler {
   }
 
   getFullList () {
+    axios
+      .get(`http://localhost:8000/characters/`)
+      .then(response => {
+        console.log('getting all characters', response.data);
+      })
+      .catch(err => {
+        if (err.response.status === 404) {
+          const errorDiv = document.createElement('div');
+          errorDiv.innerHTML = "Error: bad data";
+          document.body.appendChild(errorDiv);
+        }
+      });
 
   }
 
-  getOneRegister () {
-
+  getOneRegister (characterId) {
+    axios
+      .get(`http://localhost:8000/characters/${characterId}`)
+      .then(response => {
+        console.log(`Returning data about charachter ID ${characterId}: `, response.data);
+      })
+      .catch(err => {
+        if (err.response.status === 404) {
+          const errorDiv = document.createElement('div');
+          errorDiv.innerHTML = "There's no character with ID: " + characterId;
+          document.body.appendChild(errorDiv);
+        }
+      });
   }
 
-  createOneRegister () {
+  createOneRegister (character) {
+    axios
+      .post(`http://localhost:8000/characters/`, character)
+      .then(response => {
+        console.log('New character created: ',response.data);
+        // const {
+        //   name,
+        //   id
+        // } = response.data;
 
+        // const newCharacterHTML = `
+        //     <li>
+        //     <h3>${name}</h3>
+        //     <p>${id}</p>
+        //     `;
+
+        // document.getElementById('characterList').innerHTML += newCharacterHTML;
+
+        // document.getElementById('createForm').reset();
+      })
+      .catch(err => console.log(err));
+    }
+
+  updateOneRegister (character) {
+    axios
+      .patch(`http://localhost:8000/characters/${character.id}`, character)
+      .then(response => {
+        console.log(`charachter ID ${character.id} has been updated:`, response.data);
+      })
+      .catch(err => {
+        if (err.response.status === 404) {
+          const errorDiv = document.createElement('div');
+          errorDiv.innerHTML = "There's no character with ID: " + characterId;
+          document.body.appendChild(errorDiv);
+        }
+      });
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (characterId) {
+    axios
+      .delete(`http://localhost:8000/characters/${characterId}`)
+      .then(response => {
+        console.log(`Charachter ID ${characterId} has been deleted:`, response.data);
+      })
+      .catch(err => {
+        if (err.response.status === 404) {
+          const errorDiv = document.createElement('div');
+          errorDiv.innerHTML = "There's no character with ID: " + characterId;
+          document.body.appendChild(errorDiv);
+        }
+      });
 
   }
 }
+
+
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,46 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    const characterId = document.getElementsByName('character-id')[0].value;
+    console.log(characterId);
+    charactersAPI.getOneRegister(characterId);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    const characterId = document.getElementsByName('character-id-delete')[0].value;
+    console.log(characterId);
+    charactersAPI.deleteOneRegister(characterId);
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const character = {
+      id: document.getElementsByName('chr-id')[0].value,
+      name: document.getElementsByName('name')[1].value,
+      occupation: document.getElementsByName('occupation')[1].value,
+      weapon: document.getElementsByName('weapon')[1].value,
+      cartoon: document.getElementsByName('cartoon')[1].checked
+    };
+    charactersAPI.updateOneRegister(character);
+    document.getElementById('edit-character-form').reset();
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const character = {
+      name: document.getElementsByName('name')[0].value,
+      occupation: document.getElementsByName('occupation')[0].value,
+      weapon: document.getElementsByName('weapon')[0].value,
+      cartoon: document.getElementsByName('cartoon')[0].checked
+    };
+    charactersAPI.createOneRegister(character);
+    document.getElementById('new-character-form').reset();
   });
 });
